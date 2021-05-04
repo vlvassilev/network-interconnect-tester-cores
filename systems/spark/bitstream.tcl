b3358ec4b080796eb2a715c4deb1a86131d98032
Vladimir Vassilev <vladimir@lightside-instruments.com>
2021-05-04 13:27:49 +0200

    Release 0.21

--- systems/spark/bitstream.tcl
+++ systems/spark/bitstream.tcl
@@ -3,4 +3,5 @@ open_project spark.xpr
 update_compile_order -fileset sources_1
 launch_runs impl_1 -to_step write_bitstream -jobs 2
 wait_on_run impl_1
+if ([string compare [get_property STATUS [get_runs impl_1]] "write_bitstream Complete!"]) { exit -1 }
 write_hw_platform -fixed -force -file spark_wrapper.xsa
